b470c9d3c1f8b90498b7f4091f5e11a3a874bed6
cao-awa <cao.awa.www@gmail.com>
2024-05-11 00:20:02 +0800

    Import for sinuatum.

--- src/main/java/com/github/cao/awa/sinuatum/function/ecception/runnable/ExceptingRunnable.java
+++
@@ -1,9 +0,0 @@
-package com.github.cao.awa.sinuatum.function.ecception.runnable;
-
-@FunctionalInterface
-public interface ExceptingRunnable<EX extends Throwable> {
-    /**
-     * Runs this operation.
-     */
-    void run() throws EX;
-}
